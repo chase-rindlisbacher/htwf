a6cbaf11f09c4ceb8f23165bdab6337a560e1d4f
chase.rindlisbacher <cjrjazz20@gmail.com>
2022-12-06 16:35:15 -0700

    urls

--- Htwf/Htwf/urls.py
+++ Htwf/Htwf/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('whatilearned.urls')),
+    path('', include('what_i_learned.urls')),
 ]
